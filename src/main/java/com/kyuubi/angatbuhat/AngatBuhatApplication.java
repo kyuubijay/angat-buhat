99537f36254f44ce32aa0fdf9b87a0efc47e3ab5
jaymark@zorin <4819361+kyuubijay@users.noreply.github.com>
2025-10-15 16:27:56 +0800

    Initial Spring Boot module

---
+++ src/main/java/com/kyuubi/angatbuhat/AngatBuhatApplication.java
@@ -0,0 +1,13 @@
+package com.kyuubi.angatbuhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngatBuhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngatBuhatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyuubi/angatbuhat/AngatBuhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyuubi.angatbuhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngatBuhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
